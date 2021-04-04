d2a7f7cd499da353e81060f312d42d3726c6b54b
Vadim Scherbak <you@example.com>
2021-04-04 23:07:44 +0300

    issue 514 fix

--- core/budfox/heart.js
+++ core/budfox/heart.js
@@ -28,10 +28,20 @@ Heart.prototype.pump = function() {
 
 Heart.prototype.tick = function() {
   if(this.lastTick) {
+    var CandleSize = 1;
+    if (util.getConfig().tradingAdvisor.candleSize) { 
+      CandleSize = util.getConfig().tradingAdvisor.candleSize;
+    }
+    // make sure the last tick happened within the last candle
+    // @link #514
     // make sure the last tick happened not to lang ago
     // @link https://github.com/askmike/gekko/issues/514
-    if(this.lastTick < moment().unix() - TICKRATE * 3)
+    if(this.lastTick < moment().unix() - TICKRATE * 3) {
+      log.debug('tick in time, see #514 for details.');
+    }
+    if(this.lastTick < moment().unix() - CandleSize * 60) {
       util.die('Failed to tick in time, see https://github.com/askmike/gekko/issues/514 for details', true);
+    }
   }
 
   this.lastTick = moment().unix();
